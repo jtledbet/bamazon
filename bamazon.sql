9d87558b92cb1a8bca6aeecb50e2eae2d2a3e972
Jon <jon.ledbetter@gmail.com>
2019-04-30 17:14:45 -0400

    completed mvp

---
+++ bamazon.sql
@@ -0,0 +1,25 @@
+
+DROP DATABASE IF EXISTS bamazon_db;
+CREATE DATABASE bamazon_db;
+USE bamazon_db;
+
+CREATE TABLE products (
+  id INTEGER(12) AUTO_INCREMENT NOT NULL,
+  product_name VARCHAR(20) NOT NULL,
+  department_name VARCHAR(20) NOT NULL,
+  price DECIMAL(19, 4) NOT NULL,
+  stock_quantity INTEGER(6),
+  PRIMARY KEY (id)
+);
+
+INSERT INTO products (product_name, department_name, price, stock_quantity)
+VALUES 	("apples", "produce", 0.25, 50),
+        ("bananas (bunch)", "produce", 1.15, 25),
+        ("oranges", "produce", 0.35, 50),
+        ("plums", "produce", 0.45, 75),
+        ("limes", "produce", 0.35, 100),
+        ("hammers", "hardware", 9.95, 10),
+        ("wrenches", "hardware", 7.75, 12),
+        ("screwdrivers", "hardware", 4.99, 20),
+        ("tape measures", "hardware", 3.75, 12),
+        ("nails (box of 100)", "hardware", 3.75, 100);
---
+++ bamazon_Customer.js
@@ -0,0 +1,142 @@
+'use strict';
+
+const mysql = require("mysql");
+const { table } = require("table");
+const inquirer = require("inquirer");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+
+  // Your port; if not 3306
+  port: 3306,
+
+  // Your username
+  user: "root",
+
+  // Your password
+  password: "root",
+  database: "bamazon_db"
+});
+
+
+// connect to the mysql server and sql database
+connection.connect(function (err) {
+  if (err) throw err;
+  // run the afterConnection function after the connection is made to prompt the user
+  console.log("connected as id " + connection.threadId);
+  displayMenu();
+});
+
+var totalCost = 0;
+
+function displayMenu() {
+  var query = "SELECT id, product_name, price FROM products"
+  connection.query(query, function (err, res) {
+    if (err) throw err;
+    console.log(res);
+
+    console.log(`id   product  price`);
+    for (i = 0; i < res.length; i++) {
+      console.log(`${('0' + res[i].id).slice(-2)}   ${res[i].product_name}    ${res[i].price}`)
+    }
+
+    // console.table(res);
+
+    var maxID = 0;
+    for (var i = 0; i < res.length; i++) {
+      if (res[i].id > maxID)
+        maxID = res[i].id;
+    }
+
+    inquirer.prompt([
+      {
+        name: "productSelectionIndex",
+        type: "input",
+        message: "What would you like to purchase? (by ID) ",
+        validate: function (value) {
+          var valid = !isNaN(parseFloat(value));
+          return valid || 'Please enter a number';
+        },
+      },
+      {
+        name: "desiredQuantity",
+        type: "input",
+        message: "How many would you like?",
+        validate: function (value) {
+          var valid = !isNaN(parseFloat(value));
+          return valid || 'Please enter a number';
+        },
+      }
+    ]).then(function (answers) {
+
+      console.log(answers);
+      var productID = answers.productSelectionIndex;
+      var desiredQuantity = answers.desiredQuantity;
+
+      if (productID > maxID) {
+        console.log ("Invalid product ID number. Quitting.")
+        connection.end();
+        return;
+      }
+
+      var query = ("SELECT product_name, stock_quantity FROM products WHERE id = " + productID)
+        connection.query(query, function (err, res) {
+          if (err) throw err;
+
+          // console.log(res);
+          console.log(res[0].stock_quantity);
+          var desiredProduct = res[0].product_name;
+          var availableQuantity = res[0].stock_quantity;
+
+          // Not enough of product in stock:
+          if (desiredQuantity > availableQuantity) {
+            console.log("Insufficient quantity available in stock!")
+            console.log("Only " + availableQuantity + " " + desiredProduct + " remain.")
+            buySomethingElse();
+          } else {
+            console.log("Okay, lemme go get your " + desiredProduct + ".");
+
+            // Update stock quantities:
+            var newQuantity = availableQuantity - desiredQuantity;
+            var query = "UPDATE products SET stock_quantity = " + newQuantity + " WHERE id = " + productID;
+            connection.query(query, function (err, res) {
+              if (err) throw err;
+            })
+
+            // Calculate total cost:
+            var query = "SELECT id, product_name, price FROM products"
+            connection.query(query, function (err, res) {
+              if (err) throw err;
+
+              var unitPrice = res[productID - 1].price;
+              var total = (unitPrice * desiredQuantity);
+              totalCost += total;
+
+              console.log (unitPrice + " * " + desiredQuantity + " = " + total)
+              console.log ("Your total purchase price was $" + total + ".");
+              console.log ("You have spent a total of $" + totalCost + ".");
+              buySomethingElse();
+            })
+          };
+        });
+    });
+  })
+}  
+
+function buySomethingElse () {
+  inquirer.prompt({
+    name: "continue",
+    type: "confirm",
+    message: "Would you like to buy something else? ",
+    default: true
+  }).then(function (answer) {
+    console.log(answer)
+    if (answer.continue) {
+      displayMenu();
+    }
+    else {
+      connection.end();
+      return;
+    }
+  })
+}
